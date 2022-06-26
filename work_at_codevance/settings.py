42d7b653124851d23ffd64145a828e8472f1c4e0
mrb <matheus@dmenos.com.br>
2022-06-26 18:28:30 -0300

    Configura login e logout dos usuários
    close
    #17

--- work_at_codevance/settings.py
+++ work_at_codevance/settings.py
@@ -133,3 +133,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MODEL USER
+
+AUTH_USER_MODEL = 'base.User'
+
+# LOGIN
+
+LOGIN_URL = '/login'

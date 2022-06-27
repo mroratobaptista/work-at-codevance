4d21784c537b9e94bfeab7bc70f72757734fa1f0
mrb <matheus@dmenos.com.br>
2022-06-27 11:00:37 -0300

    Cria checagem de pagamentos com data de vencimento igual a "hoje"
    close
    #34

--- work_at_codevance/base/tasks.py
+++ work_at_codevance/base/tasks.py
@@ -1,8 +1,26 @@
+import logging
+from datetime import datetime
+
+from celery import current_app
 from celery import shared_task
 from django.core import mail
 from django.template.loader import render_to_string
 
+from work_at_codevance.base.models import Payment
 from work_at_codevance.settings import DEFAULT_FROM_EMAIL
+
+
+@current_app.task
+def check_payments_due_today():
+    status = 'DISPO AGUAR'.split()
+    payments = Payment.objects.filter(date_due=datetime.today(), status__in=status)
+    for p in payments:
+        p.status = 'INDIS'
+        p.save()
+
+        logger = logging.getLogger('db')
+        msg = f'ID Pagamento: {p.id} ficou indisponível para adiantamento, pois a data de vencimento é hoje.'
+        logger.info(msg)
 
 
 @shared_task
--- work_at_codevance/celery.py
+++ work_at_codevance/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'work_at_codevance.settings')
@@ -15,6 +16,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from work_at_codevance.base.tasks import check_payments_due_today
+    sender.add_periodic_task(crontab(hour=0, minute=0), check_payments_due_today.s())
 
 
 @app.task(bind=True)
